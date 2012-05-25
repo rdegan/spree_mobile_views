cb10f89591568525638c22ee7bee77b5f22fe139
Riccardo Degan <riccardo.degan@diginess.it>
2012-05-25 12:52:25 +0200

    Add MOBILE_USER_AGENTS variable in Initializers file

--- lib/spree_mobile_views.rb
+++ lib/spree_mobile_views.rb
@@ -20,11 +20,11 @@ end
 module SpreeBase
   module InstanceMethods
     protected
-    MOBILE_USER_AGENTS = 'palm|blackberry|nokia|phone|midp|mobi|symbian|chtml|ericsson|minimo|' +
-                         'audiovox|motorola|samsung|telit|upg1|windows ce|ucweb|astel|plucker|' +
-                         'x320|x240|j2me|sgh|portable|sprint|docomo|kddi|softbank|android|mmp|' +
-                         'pdxgw|netfront|xiino|vodafone|portalmmm|sagem|mot-|sie-|ipod|up\\.b|' +
-                         'webos|amoi|novarra|cdm|alcatel|pocket|ipad|iphone|mobileexplorer|mobile'
+    # MOBILE_USER_AGENTS = 'palm|blackberry|nokia|phone|midp|mobi|symbian|chtml|ericsson|minimo|' +
+    #                      'audiovox|motorola|samsung|telit|upg1|windows ce|ucweb|astel|plucker|' +
+    #                      'x320|x240|j2me|sgh|portable|sprint|docomo|kddi|softbank|android|mmp|' +
+    #                      'pdxgw|netfront|xiino|vodafone|portalmmm|sagem|mot-|sie-|ipod|up\\.b|' +
+    #                      'webos|amoi|novarra|cdm|alcatel|pocket|ipad|iphone|mobileexplorer|mobile'
     
     def mobile_device?
       if cookies[:mobile_param]
